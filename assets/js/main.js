72f1e8b1a12f84524bccc4915cd4c9a0bfa67eb8
Shreehar-KE <shreehar.elancheran@gmail.com>
2024-08-12 13:33:55 +0530

    complete about js

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+// SHOW MENU
+
+
+// MENU
+// VALIDATE IF CONSTANT EXISTS
+
+
+// MENU HIDDEN
+// VALIDATE IF CONSTANT EXISTS
+
+
+// REMOVE MENU MOBILE
+
+
+// SCROLL SECTIONS ACTIVE LINK
+
+
+// CHANGE BACKGROUND HEADER
+
+
+// SHOW SCROLL UP
+
+
+// ABOUT TABS
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click',()=> {
+        const target = document.querySelector(tab.dataset.target);
+        
+        tabContents.forEach((tabContent) => {
+            tabContent.classList.remove('tab__active');
+        });
+
+        target.classList.add('tab__active');
+
+        tabs.forEach((tab) => {
+            tab.classList.remove('tab__active');
+        });
+
+        tab.classList.add('tab__active');
+    });
+});
+
+// CONTACT FORM
