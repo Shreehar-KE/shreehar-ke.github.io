e1f69f8705d398dc1ae2ef74cb5f808ef17d0cbb
Shreehar-KE <shreehar.elancheran@gmail.com>
2024-08-14 23:55:43 +0530

    complete contact js

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,3 +44,49 @@ tabs.forEach((tab) => {
 });
 
 // CONTACT FORM
+const contactForm = document.getElementById('contact-form'),
+contactName = document.getElementById('contact-name'),
+contactEmail = document.getElementById('contact-email'),
+contactSubject = document.getElementById('contact-subject'),
+contactMessage = document.getElementById('contact-message'),
+errorMessage = document.getElementById('error-message');
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    // check if the field has a value
+    if (contactName.value === '' || contactEmail.value === '' || contactSubject.value === '' || contactMessage.value === '') {
+        // show message
+        errorMessage.textContent = 'Write all the input fields'
+    } 
+    else {
+        // serviceID - templateID - #form - publickey
+        emailjs.sendForm(
+            'service_8hh0s46', 
+            'template_uer5rdp', 
+            '#contact-form', 
+            'dNGPgenuXyHnG_sM9'
+        ).then(() => {
+            // show message and add color, window + dot to open emoji
+            errorMessage.classList.add('color-first');
+            errorMessage.textContent = 'Message Sent ✔'
+
+            // remove message after 5 seconds
+            setTimeout(() => {
+                errorMessage.textContent = '';
+            
+            }, 5000);
+        }, (error) => {
+            alert('OOPs!, SOMETHING WENT WRONG...', error);
+        });
+
+        //clear input fields
+        contactName.value = ''
+        contactEmail.value = ''
+        contactSubject.value = ''
+        contactMessage.value = ''
+    }
+};
+
+contactForm.addEventListener('submit',  sendEmail);
+
