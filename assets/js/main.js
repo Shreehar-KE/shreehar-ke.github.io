71086c36333d2c2b718462901a1a3b57f9a4dd41
Shreehar-KE <shreehar.elancheran@gmail.com>
2024-08-21 23:18:42 +0530

    complete navbar-menu js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,44 @@
 // SHOW MENU
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
 
-
-// MENU
+// MENU SHOW
 // VALIDATE IF CONSTANT EXISTS
-
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
 
 // MENU HIDDEN
 // VALIDATE IF CONSTANT EXISTS
-
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
 
 // REMOVE MENU MOBILE
+const navLink = document.querySelectorAll('.nav__link');
 
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach((n) => n.addEventListener('click', linkAction));
 
 // SCROLL SECTIONS ACTIVE LINK
-
+navLink.forEach((n) => {
+    n.addEventListener('click', (event) => {
+        const target = event.target;
+        navLink.forEach((link) => {
+            link.classList.remove('active-link');
+        });
+        target.classList.add('active-link');
+    });
+});
 
 // CHANGE BACKGROUND HEADER
 function scrollHeader() {
