25eda51b679e70c798b3ecb13f682f65724ccb64
Shreehar-KE <shreehar.elancheran@gmail.com>
2024-10-31 04:57:55 +0530

    update projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,61 +1,61 @@
 // SHOW MENU
 const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close');
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
 
 // MENU SHOW
 // VALIDATE IF CONSTANT EXISTS
 if (navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu');
-    });
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
 }
 
 // MENU HIDDEN
 // VALIDATE IF CONSTANT EXISTS
 if (navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu');
-    });
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
 }
 
 // REMOVE MENU MOBILE
 const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction() {
-    const navMenu = document.getElementById('nav-menu');
-    navMenu.classList.remove('show-menu');
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show-menu');
 }
 
 navLink.forEach((n) => n.addEventListener('click', linkAction));
 
 // SCROLL SECTIONS ACTIVE LINK
 navLink.forEach((n) => {
-    n.addEventListener('click', (event) => {
-        const target = event.target;
-        navLink.forEach((link) => {
-            link.classList.remove('active-link');
-        });
-        target.classList.add('active-link');
+  n.addEventListener('click', (event) => {
+    const target = event.target;
+    navLink.forEach((link) => {
+      link.classList.remove('active-link');
     });
+    target.classList.add('active-link');
+  });
 });
 
 // CHANGE BACKGROUND HEADER
 function scrollHeader() {
-    const header = document.getElementById('header');
-    // when the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
-    if (this.scrollY >= 80) header.classList.add('scroll-header');
-    else header.classList.remove('scroll-header');
+  const header = document.getElementById('header');
+  // when the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 80) header.classList.add('scroll-header');
+  else header.classList.remove('scroll-header');
 }
 
 window.addEventListener('scroll', scrollHeader);
 
 // SHOW SCROLL UP
 function scrollUp() {
-    const scrollUp = document.getElementById('scroll-up');
-    // when the scroll is greater than 350 viewport height, add the show-scroll class to the scrollUp tag
-    if (this.scrollY >= 350) scrollUp.classList.add('show-scroll');
-    else scrollUp.classList.remove('show-scroll');
+  const scrollUp = document.getElementById('scroll-up');
+  // when the scroll is greater than 350 viewport height, add the show-scroll class to the scrollUp tag
+  if (this.scrollY >= 350) scrollUp.classList.add('show-scroll');
+  else scrollUp.classList.remove('show-scroll');
 }
 
 window.addEventListener('scroll', scrollUp);
@@ -63,70 +63,72 @@ window.addEventListener('scroll', scrollUp);
 
 // ABOUT TABS
 const tabs = document.querySelectorAll('[data-target]'),
-    tabContents = document.querySelectorAll('[data-content]');
+  tabContents = document.querySelectorAll('[data-content]');
 
 tabs.forEach((tab) => {
-    tab.addEventListener('click',()=> {
-        const target = document.querySelector(tab.dataset.target);
-        
-        tabContents.forEach((tabContent) => {
-            tabContent.classList.remove('tab__active');
-        });
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target);
 
-        target.classList.add('tab__active');
-
-        tabs.forEach((tab) => {
-            tab.classList.remove('tab__active');
-        });
-
-        tab.classList.add('tab__active');
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove('tab__active');
     });
+
+    target.classList.add('tab__active');
+
+    tabs.forEach((tab) => {
+      tab.classList.remove('tab__active');
+    });
+
+    tab.classList.add('tab__active');
+  });
 });
+
+
 
 // CONTACT FORM
 const contactForm = document.getElementById('contact-form'),
-contactName = document.getElementById('contact-name'),
-contactEmail = document.getElementById('contact-email'),
-contactSubject = document.getElementById('contact-subject'),
-contactMessage = document.getElementById('contact-message'),
-errorMessage = document.getElementById('error-message');
+  contactName = document.getElementById('contact-name'),
+  contactEmail = document.getElementById('contact-email'),
+  contactSubject = document.getElementById('contact-subject'),
+  contactMessage = document.getElementById('contact-message'),
+  errorMessage = document.getElementById('error-message');
 
 const sendEmail = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    // check if the field has a value
-    if (contactName.value === '' || contactEmail.value === '' || contactSubject.value === '' || contactMessage.value === '') {
-        // show message
-        errorMessage.textContent = 'Write all the input fields'
-    } 
-    else {
-        // serviceID - templateID - #form - publickey
-        emailjs.sendForm(
-            'service_8hh0s46', 
-            'template_uer5rdp', 
-            '#contact-form', 
-            'dNGPgenuXyHnG_sM9'
-        ).then(() => {
-            // show message and add color
-            errorMessage.classList.add('color-first');
-            errorMessage.textContent = 'Message Sent ✔'
+  // check if the field has a value
+  if (contactName.value === '' || contactEmail.value === '' || contactSubject.value === '' || contactMessage.value === '') {
+    // show message
+    errorMessage.textContent = 'Write all the input fields'
+  }
+  else {
+    // serviceID - templateID - #form - publickey
+    emailjs.sendForm(
+      'service_8hh0s46',
+      'template_uer5rdp',
+      '#contact-form',
+      'dNGPgenuXyHnG_sM9'
+    ).then(() => {
+      // show message and add color
+      errorMessage.classList.add('color-first');
+      errorMessage.textContent = 'Message Sent ✔'
 
-            // remove message after 5 seconds
-            setTimeout(() => {
-                errorMessage.textContent = '';
-            
-            }, 5000);
-        }, (error) => {
-            alert('OOPs!, SOMETHING WENT WRONG...', error);
-        });
+      // remove message after 5 seconds
+      setTimeout(() => {
+        errorMessage.textContent = '';
 
-        //clear input fields
-        contactName.value = ''
-        contactEmail.value = ''
-        contactSubject.value = ''
-        contactMessage.value = ''
-    }
+      }, 5000);
+    }, (error) => {
+      alert('OOPs!, SOMETHING WENT WRONG...', error);
+    });
+
+    //clear input fields
+    contactName.value = ''
+    contactEmail.value = ''
+    contactSubject.value = ''
+    contactMessage.value = ''
+  }
 };
 
-contactForm.addEventListener('submit',  sendEmail);
+contactForm.addEventListener('submit', sendEmail);
 
